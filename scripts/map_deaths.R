4c34757646ddc2d53f043d162afea3f605809f14
Christian Testa <ctesta01@users.noreply.github.com>
2021-09-07 13:49:24 -0400

    Delete map_deaths.R

--- scripts/map_deaths.R
+++
@@ -1,8 +0,0 @@
-source(here::here('scripts/dependencies.R'))
-source(here::here('scripts/read_cook_cty_deaths.R'))
-source(here::here('scripts/make_tract_mort_w_absms.R'))
-
-options(tigris_use_cache=TRUE) 
-
-map_overall_tract_mortality_rates()
-                              
